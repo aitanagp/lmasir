3fada353de57ffc4e8888476bd961bcf976490e9
aitanagp <165787766+aitanagp@users.noreply.github.com>
2024-09-17 09:55:24 +0200

    Add files via upload

---
+++ primer trimestre/practica_2.4_AitanaGP_24/Solucion p2.4/rb_formsubmit.js
@@ -0,0 +1,15 @@
+/*
+
+
+   Filename: rb_formsubmit.js
+
+*/
+
+window.onload = setForm;
+
+function setForm() {
+   document.forms[0].onsubmit = function() {
+      if (this.checkValidity()) alert("No invalid data detected. Will retain data for further testing.");
+      return false;
+   }
+}
